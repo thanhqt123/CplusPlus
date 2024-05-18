a1b4fab59cb1c09513f59f6dc4b57097665eae30
thanhqt123 <142771633+thanhqt123@users.noreply.github.com>
2024-05-18 23:01:30 +0700

    Dem tu trong cau truc
    Dem tu trong cau truc

--- string1.cpp
+++ string1.cpp
@@ -1,45 +1,59 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
-int solve(string a[], int n) {
-    int cnt = 0;
-    bool appeared[10000] = {false}; 
-    for (int i = 0; i < n; ++i) {
-        if (!appeared[i]) {
-            appeared[i] = true; 
-            ++cnt; 
-        
-            for (int j = i + 1; j < n; ++j) {
-                if (a[i] == a[j]) {
-                    appeared[j] = true;
-                }
-            }
-        }
+struct word {
+    string data;
+    int fre;
+};
+
+// Hàm so sánh để sắp xếp các từ theo số lần xuất hiện giảm dần, nếu bằng nhau thì theo thứ tự từ điển
+bool compare(word a, word b) {
+    if (a.fre == b.fre) {
+        return a.data < b.data;
+    } else {
+        return a.fre > b.fre;
     }
-    return cnt;
 }
 
 int main() {
-    string s;
-    getline(cin, s);
-
+    const int max_words = 1000;
+    word words[max_words];
+    int index = 0;
     
-    for (char& c : s) {
-        c = tolower(c);
+    string line, word;
+    
+    // Đọc và xử lý đầu vào
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        while (ss >> word) {
+            // Tìm từ này trong mảng
+            bool found = false;
+            for (int i = 0; i < index; ++i) {
+                if (words[i].data == word) {
+                    words[i].fre++;
+                    found = true;
+                    break;
+                }
+            }
+            // Nếu không tìm thấy thì thêm từ vào mảng
+            if (!found) {
+                words[index].data = word;
+                words[index].fre = 1;
+                index++;
+            }
+        }
     }
-
-    stringstream ss(s);
-    const int wordMax = 10000;
-    string words[wordMax];
-    int wc = 0;
-    string w;
-
-    while (ss >> w && wc < wordMax) {
-        words[wc++] = w;
+    
+    // Sắp xếp mảng theo số lần xuất hiện giảm dần và theo thứ tự từ điển
+    sort(words, words + index, compare);
+    
+    // In ra từ xuất hiện nhiều nhất
+    if (index > 0) {
+        cout << words[0].data << endl;
     }
-
-    cout << solve(words, wc);
+    
     return 0;
 }
